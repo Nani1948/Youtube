b3b1197dd47b1d263a57723a2ed22ae0fa0adeb9
Nani1948 <nandhinikaluvakollu18@gmail.com>
2026-09-08 20:46:48 +0530

    Update model video

--- models/Video.js
+++ models/Video.js
@@ -3,75 +3,81 @@ import mongoose from "mongoose";
 
 // Create Video Schema
 const videoSchema = new mongoose.Schema({
-
+    //Create a Video ID
     videoId: {
         type: String,
         required: [true, "Video ID is required"],
         unique: true,
         trim: true,
-         default: () => new mongoose.Types.ObjectId().toString()
+        default: () => new mongoose.Types.ObjectId().toString()
     },
-
+    //Title video
     title: {
         type: String,
         required: [true, "Video title is required"],
         trim: true
     },
-
+    //Thumbnail URL
     thumbnailUrl: {
         type: String,
         required: [true, "Thumbnail URL is required"],
         trim: true
     },
-
+    //Video URL
     videoUrl: {
         type: String,
         required: [true, "Video URL is required"],
         trim: true
     },
-
+    //Descripition
     description: {
         type: String,
         required: [true, "Video description is required"],
         trim: true
     },
-
+    // Video category
+    category: {
+        type: String,
+        required: [true, "Video category is required"],
+        trim: true
+    },
+    //Channel ID
     channelId: {
         type: String,
         required: [true, "Channel ID is required"],
         trim: true
     },
-
+    //Upload video
     uploader: {
         type: String,
         required: [true, "Uploader is required"],
         trim: true
     },
-
+    //View video
     views: {
         type: Number,
         default: 0
     },
-
+    //Like video
     likes: {
         type: Number,
         default: 0
     },
-
+    //Dislike video
     dislikes: {
         type: Number,
         default: 0
     },
-
+    //Upload date
     uploadDate: {
         type: Date,
         required: [true, "Upload date is required"]
     },
-    
+
     // Pinned video
-    isPinned:{
-        type:Boolean,
-        default:false
+    isPinned: {
+        type: Boolean,
+        default: false
     },
     // Reference Comment model
     comments: [
